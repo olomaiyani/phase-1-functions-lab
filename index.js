8301aff5c1ddad6e9692b12e41aad799a37b2c52
+ <+>
2022-08-17 12:02:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(value){
+if (value>42){
+    return value-42;
+}else{
+    return 42-value;
+}
+}
+function distanceFromHqInFeet(value){
+    return distanceFromHqInBlocks(value)*264
+}
+function distanceTravelledInFeet(destination,start){
+    return Math.abs(destination-start)*264
+}
+function calculatesFarePrice(start, destination){
+
+    if(distanceTravelledInFeet(start,destination)<400){
+        return 0;
+    }
+    else if ((distanceTravelledInFeet(start,destination)<=2000)*2.56){
+        return ((distanceTravelledInFeet(start,destination)-400)/50);
+    }
+     else if (distanceTravelledInFeet(start,destination)>2500){
+     return"cannot travel that far";
+    }
+    else{
+        return 25;
+    }
+}
